d6bfeec4b7cd01dc47be8f9fc803258562684e6e
980f <andy.980f@gmail.com>
2018-12-10 15:44:38 -0600

    partitioned telnetter

--- telnetter.h
+++ telnetter.h
@@ -3,18 +3,8 @@
 
 #include "chainprinter.h"
 ChainPrinter dbg(debug);
- 
+
 #include "char.h"  //until chainprinter prints hex.
-
-//unsigned hexify(uint8_t charish){
-//  unsigned fourchars; //sizeof unsigned
-//  Char see(charish);
-//  //todo; endinaness
-//  fourchars=see.hexnibble(0)+(see.hexnibble(1)<<8);
-//  return fourchars;
-//}
-
-
 
 //max number of simultaneous clients allowed, note: their incoming stuff is mixed together. using 3 as the 8266 supports at most 4 sockets.
 #define MAX_SRV_CLIENTS 3
@@ -22,7 +12,7 @@ ChainPrinter dbg(debug);
 struct Credentials {//todo: use allocation constants from wifi.h
   char ssid[32];
   char password[64];
-  
+
   unsigned save(unsigned offset = 0) { //todo: symbols or static allocator.
     return strzsave(password, sizeof(password), strzsave(ssid, sizeof(ssid), offset));
   }
@@ -42,17 +32,31 @@ struct Credentials {//todo: use allocation constants from wifi.h
 
 };
 
-const char *Hostname = "980FMesher";
 
 using MAC = uint8_t[WL_MAC_ADDR_LENGTH];
+
+struct TelnetActor {
+  virtual void onInput(uint8_t *bytes, unsigned length, unsigned ci) {
+    ;//do nothing
+  }
+
+  virtual void onConnect(WiFiClient &aclient, unsigned ci) {
+    ;//do nothing
+  }
+
+  virtual const char *hostname() {
+    return nullptr;
+  }
+
+};
+
 
 //will make into its own module real soon now.
 struct Telnetter {
   bool amTrying = false;
   bool amConnected = false;
   bool beTrying = false;
-  /** whether to send data from one wifi to all others as well as serial */
-  bool chat = true;
+
   uint16_t teleport;//retained for diagnostics
   MonoStable testRate;
 
@@ -63,10 +67,13 @@ struct Telnetter {
   WiFiServer server;
   WiFiClient serverClients[MAX_SRV_CLIENTS];
 
-  Telnetter(uint16_t teleport = 23): //23 is standard port, need to make this a configurable param so that we can serve different processes on one device.
+  TelnetActor *actor;
+
+  Telnetter(TelnetActor *actor, uint16_t teleport = 23): //23 is standard port
     teleport(teleport),
     testRate(500), //500=2 Hz
-    server(teleport) {
+    server(teleport),
+    actor(actor) {
     //#done
   }
 
@@ -77,10 +84,12 @@ struct Telnetter {
     return tryConnect();//early first attempt, not strictly needed.
   }
 
+
   /** call this frequently from loop()
+      calls back with chunks from clients.
       Feeds merged wifi data from all clients to the given stream.
   */
-  bool serve(Stream &stream) {//todo: use functional that matches Stream::write(uint8*,unsigned)
+  bool serve( ) {//todo: use functional that matches Stream::write(uint8*,unsigned)
     if (amConnected) {
       if (server.hasClient()) {//true when a client has requested connection
         if (!findSlot()) {//no free/disconnected spot so reject
@@ -97,9 +106,8 @@ struct Telnetter {
           size_t len = aclient.available();
           uint8_t sbuf[len];//not standard C++! (but most compilers will let you do it)
           aclient.readBytes(sbuf, len);
-          stream.write(sbuf, len);
-          if (chat) {
-            broadcast(*sbuf, len, ci);
+          if (actor) {
+            actor->onInput(sbuf, len, ci);
           }
         }
       }
@@ -142,19 +150,19 @@ struct Telnetter {
   bool tryConnect() {
     if (cred) {
       WiFi.mode(WIFI_STA);
-      WiFi.hostname(Hostname);
+      WiFi.hostname(actor ? actor->hostname() : "Voldemort");//' Voldemort' he who shall not be named
       WiFi.begin(cred->ssid, cred->password);
-     
+
       dbg("\nDevice Mac ");
       MAC mac;
       WiFi.macAddress(mac);
       for (unsigned mi = 0; mi < WL_MAC_ADDR_LENGTH; mi++) {
         Char c(mac[mi]);
-        dbg(':',c.hexNibble(1),c.hexNibble(0));
+        dbg(':', c.hexNibble(1), c.hexNibble(0));
       }
-      dbg("\nConnecting to AP ",cred->ssid);
+      dbg("\nConnecting to AP ", cred->ssid);
       if (Verbose) {
-        dbg(" using password ",cred->password);
+        dbg(" using password ", cred->password);
       }
       amTrying = true;
     } else {
@@ -165,12 +173,12 @@ struct Telnetter {
 
   bool testConnection() {
     wst = WiFi.status();
-    dbg("\tAPCon status ",wst);
+    dbg("\tAPCon status ", wst);
     if (wst == WL_CONNECTED) {
       server.begin();
       server.setNoDelay(true);
 
-      dbg("\nUse 'telnet ",WiFi.localIP());
+      dbg("\nUse 'telnet ", WiFi.localIP());
       if (teleport != 23) {
         dbg(teleport);
       }
@@ -184,20 +192,18 @@ struct Telnetter {
 
   /** see if we have a place to keep client connection info. If so retain it.*/
   bool findSlot() {
-    for (unsigned i = MAX_SRV_CLIENTS; i-- > 0;) {
-      auto &aclient = serverClients[i];
+    for (unsigned ci = MAX_SRV_CLIENTS; ci-- > 0;) {
+      auto &aclient = serverClients[ci];
       if (aclient && !aclient.connected()) {//connection died
         aclient.stop();//clean it up.
       }
       //not an else! aclient.stop() may make aclient be false.
       if (!aclient) {//if available
         aclient = server.available();
-        dbg("\nNew client: #",i);//todo: find out how to get client ip, will need to go around wifi class proper to do so.
-
-        aclient.write(Hostname);
-        aclient.write(" at your service, station ");
-        aclient.write('0'+i);
-        aclient.write(".\n");
+        dbg("\nNew client: #", ci); //todo: find out how to get client ip, will need to go around wifi class proper to do so.
+        if (actor) {
+          actor->onConnect(aclient, ci);
+        }
         return true;
       }
     }
