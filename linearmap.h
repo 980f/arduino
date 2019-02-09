83f3c78a375e71debced93986143449aabaa8945
980f <andy.980f@gmail.com>
2019-02-09 09:46:15 -0600

    configurable use of FP in linear map

--- linearmap.h
+++ linearmap.h
@@ -1,12 +1,14 @@
 #pragma once //(C)2019 Andy Heilveil, github/980F
 
+#include "analog.h"
+
 /** like arduino's map() but with subtle syntax and one axis with a fixed 0 as the low.
- *  we also laboriously use long instead of float to save ~1.5k of code. This could be made much more efficient if we were to indulge in some asm, work for the AVR family.
+    if AvoidFP is defined to anything other than 0 then we also laboriously use long instead of float to save ~1.5k of code. This could be made much more efficient if we were to indulge in some asm, work for the AVR family.
 */
 struct LinearMap {
   unsigned top;
   unsigned bottom;
-  
+
   LinearMap(unsigned top, unsigned bottom = 0):
     top(top), bottom(bottom)  {
     //#done
@@ -14,13 +16,28 @@ struct LinearMap {
 
   /** convert fraction of range in 1.15 format to unsigned output without using floating point. */
   unsigned operator ()(AnalogValue avin, bool clipit = true)const {
+#if AvoidFP
     auto scaledup = long(top - bottom) * avin.raw;
     unsigned mapped = bottom + ((scaledup + (1 << 14)) >> 15);
+#else
+    float fract = avin.raw;
+    fract /= 32768.0; //hopefully compiler will just twiddle the exponent
+    unsigned mapped = unsigned(fract * (top - bottom) + 0.5 + bottom);
+#endif
     if (clipit) {
       return clipped(mapped);
     } else {
       return mapped;
     }
+  }
+
+  /** inverse map. @returns a number that when passed to operator() will give the number passed in. */
+  AnalogValue operator /(unsigned scaled) const {
+#if AvoidFP
+    return AnalogValue(  ((long(scaled) - bottom) << 15) / long(top - bottom)); //#casting here is crucial
+#else
+    return AnalogValue( ((float(scaled) / (top - bottom)) + bottom) * 32768.0);//todo:00 this is wrong! but I am in a hurry to get other stuff done!
+#endif
   }
 
   unsigned clipped(unsigned raw)const {
@@ -45,8 +62,5 @@ struct LinearMap {
     return false;
   }
 
-//inverse map
-  AnalogValue operator /(unsigned scaled) const {
-    return AnalogValue(  ((long(scaled)-bottom)<<15)/long(top-bottom));//#casting here is crucial
-  }
+
 };
