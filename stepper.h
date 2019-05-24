0ed5935f1dfa31be7a332314eb0168ccde0a5a6a
980f <andy.980f@gmail.com>
2019-05-24 09:41:27 -0500

    refining signature of setpper interface functional

--- stepper.h
+++ stepper.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "hook.h"
+#include "minimath.h"//signof
 
 /** nascent class for stepper interface.
   will be adding accleration and other speed management into this guy over time. */
@@ -13,19 +14,21 @@ class Stepper {
   /** assign a function which takes a nibble and routes it to your wires.*/
     Hook<byte> interface;
     using Interface=Hook<byte>::Pointer;
-    
+
+    /** @returns nominal location */
     operator int() const {
       return step;
     }
-
+    
+		/** set value as nominal location */
 		void operator =(int location){
-			stepOffset=(location^step)&3;
+			stepOffset=(location^step)&3;//need to deal with this not being zero. 
 			step=location;
       applyPhase(step);
 		}
 
-    byte applyPhase(unsigned step)const {
-      interface(step);
+    byte applyPhase(unsigned step) { //had to remove const on this method due to compiler bug, the Hook should not have inherited constness from the object.
+      interface(byte(step));
     }
 
     /** step either forward or back*/
@@ -45,7 +48,6 @@ class Stepper {
       //    if((++phase)==perRevolution){
       //      phase=0;
       //    }
-
     }
 
     void operator --() {
