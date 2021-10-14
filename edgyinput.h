345527c490f737505e5a9f191324d31aabe48ed5
980f <andy.980f@gmail.com>
2021-10-14 12:57:59 -0500

    added debouncing, can't tell if it is working correctly

--- edgyinput.h
+++ edgyinput.h
@@ -3,19 +3,42 @@
 #include "cheaptricks.h" //::changed
 
 class EdgyInput {
-    bool last;
+    bool last; //last stable value
     const DigitalInput pin;
+    unsigned debouncer;
+    unsigned filter;
   public:
-    EdgyInput(unsigned which): pin(which) {
-      last = this->operator bool();
+    EdgyInput(unsigned which, unsigned filter = 0): pin(which), filter(filter) {
+      last = this->raw();//elaborate in order to check compiler
     }
 
+
     operator bool () {
+      if (last != raw()) {
+        if (debouncer++ >= filter) {
+          last = !last;//it has changed
+        }
+      } else {
+        debouncer = 0;
+      }
+      return last;
+    }
+
+    bool raw() {
       return bool(pin);
     }
 
     bool changed() {
-      return ::changed(last, *this);
+      if (filter > 0) {
+        if (debouncer >= filter) {
+          debouncer = 0;
+          return true;
+        }
+        return false;
+      } else {
+        return ::changed(last, raw());
+      }
+
     }
 
 };
