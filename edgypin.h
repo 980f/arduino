0e8c37b68dc1cee8ae9ad32cf4c7bcdfb738b6b3
andyh <andy.980f@gmail.com>
2021-12-08 21:39:10 -0600

    newly built and untested module for debounced input pins after edgyinput was made a template

---
+++ edgypin.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "edgyinput.h"  //generic input debouncer
+#include  "digitalpin.h"
+
+/** debounced input pin. */
+class EdgyPin : public EdgyInput<bool> {
+    const DigitalInput pin;
+  public:
+    EdgyPin (unsigned arduinoNumber, DigitalPin::Datum polarity, unsigned filter): pin(arduinoNumber, polarity) {
+      configure(filter);
+    }
+
+    /** @returns whether this just changed */
+    bool onTick() {
+      return (*this)(pin);
+    }
+
+    /** call from setup() */
+    void begin() {
+      //DigitalPin inits itself, no action needed here.
+      init(pin);
+    }
+
+    bool raw() const {
+      return pin;
+    }
+};
