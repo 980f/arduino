0883a6c0552266eae1bbc249d2257664211181fc
andyh <andy.980f@gmail.com>
2022-01-24 09:28:20 -0600

    modules crafted for cleaning up ST's vl53l0x driver

---
+++ intsizer.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+namespace xstd { 
+
+//std::conditional was missing on one platform, and was getting tedious to figure out how to add this.
+
+template<bool Condition, typename ForTrue, typename ForFalse> struct conditional {
+  typedef ForTrue type;
+};
+
+template< typename ForTrue, typename ForFalse> struct conditional<false, ForTrue, ForFalse> {
+  typedef ForFalse type;
+};
+
+}
+
+
+/** provides type at least large enough for the given bits.
+   This should be somewhere in the c++ std library but I haven't found it
+ * */
+template<unsigned size> struct Unsigned {
+  using type =
+    typename xstd::conditional < size <= 8, uint8_t,
+    typename xstd::conditional < size <= 16, uint16_t,
+    typename xstd::conditional<size <= 32, uint32_t,
+    uint64_t
+    >::type
+    >::type
+    >::type;
+};
+
+template<unsigned size> struct Signed {
+  using type =
+    typename xstd::conditional < size <= 8, int8_t,
+    typename xstd::conditional < size <= 16, int16_t,
+    typename xstd::conditional<size <= 32, int32_t,
+    int64_t
+    >::type
+    >::type
+    >::type;
+};
