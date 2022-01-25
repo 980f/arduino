6c78aadc8ce506ea81620fd997565fe248017335
andyh <andy.980f@gmail.com>
2022-01-24 18:25:14 -0600

    now fully silent when all samples are 1 ms

--- millichecker.h
+++ millichecker.h
@@ -2,17 +2,6 @@
 
 #include "millievent.h"
 
-#define DebugMillichecker 0
-
-
-#if DebugMillichecker
-
-#include "chainprinter.h"
-extern ChainPrinter dbg;
-
-#else
-#define dbg(...) 
-#endif
 
 /**
   utility to see if you are missing milliseconds in your loop().
@@ -34,46 +23,12 @@ class MilliChecker {
     unsigned checks = 0; //for application to decide when to show
 
     /** call often enough to sample every millisecond value */
-    void check() {
-      unsigned delta = MilliTicker.since(millilast);
-      
-      dbg("MC:",delta,"/",millilast);
-      
-      ++ millihist[min(delta, numhist)];
-      millilast = MilliTicker.recent();
-      ++checks;
-    }
-
+    void check();
     /** shows the histogram on Print @param dbg, and if @param andClear then zero the 'gram.
     */
-    bool show(Print &dbg, bool andClear = true) {
-      unsigned nonzeroes = 0;
-      for (unsigned hi = numhist; hi > 0; --hi) { //ignore 0th entry
-        if (millihist[hi] > 0) {//zeroes are usually very common at the high end of the range so we drop all as a convenient way of dropping those.
-          //if 1 is the first non-zero entry then print nothing
-          if (hi > 1 || nonzeroes > 0) {
-            if(nonzeroes==0){
-              dbg.println();//ensure alignment of output, but only if we are definitely outputing something.
-            }
-            dbg.print(hi); dbg.print(":\t"); dbg.println(millihist[hi]);
-          }
-     
-          ++nonzeroes;
-          if (andClear) {
-            millihist[hi] = 0;
-          }
-        }
-      }
-      return nonzeroes!=0;
-    }
+    bool show(Print &dbg, bool andClear = true) ;
 
     //update and if it has been @param modulus checks show the histogram and if @param andClear then zero the 'gram.
-    bool operator ()(Print &dbg, unsigned modulus, bool andClear = true) {
-      check();
-      if (checks % modulus == 0) {
-        return show(dbg, andClear);
-      }
-      return false;
-    }
+    bool operator ()(Print &dbg, unsigned modulus, bool andClear = true);
 
 };
