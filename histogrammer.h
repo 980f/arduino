e3fd1684eae90e3be53f74cdac4f18d510664718
andyh <andy.980f@gmail.com>
2022-02-02 12:40:13 -0600

    merged in features from similar class, and applied Arduino formatter

--- histogrammer.h
+++ histogrammer.h
@@ -1,27 +1,57 @@
 #pragma once
 
-/** allocates tempalte param quanity of 0-based bins.
+/** allocates template param quanity of 0-based bins.
     call operator () to increment a bin
     call show to dump the data.
 */
 
 template <unsigned qty> class HistoGrammer {
-  unsigned hist[qty + 1]; //+1 for 'invalid'
-public:
-  unsigned operator()(unsigned value) {
-    ++hist[min(value, qty)];
-    return value;
-  }
+    unsigned hist[qty + 1]; //+1 for 'outofrange'
+    bool onebased = false;
+  public:
+    unsigned operator()(unsigned value) {
+      ++hist[min(value, qty)];
+      return value;
+    }
 
-  void show(bool skipZeroes=true,bool andClear=true) {
-    Serial.println();//ensure alignment of output
-    for (unsigned hi = qty + 1; hi-- > 0;) {
-      if (!skipZeroes || hist[hi] > 0) {
-        Serial.print(hi); Serial.print(":\t"); Serial.println(hist[hi]);
-        if (andClear) {
-          hist[hi] = 0;
+    struct ShowOptions {
+      bool andClear = true;
+      bool skipZeroes = true; //not just leading, always skips leading zeroes
+      //todo: when we implement an oor channel  bool oors=false;
+    };
+
+    void show(Print &dbg, const ShowOptions &opts) {
+      unsigned nonzeroes = 0;//for not outputting anything if whole histogram is zero
+      for (unsigned hi = numhist; hi -- > 0;) {
+        bool isZero = hist[hi] == 0;
+        if (skipZeroes && isZero) {
+          continue;
+        }
+        if (nonzeroes == 0 && isZero) { //zeroes are usually very common at the high end of the range so we drop all as a convenient way of dropping those.
+          continue;  //don't count 'leading' zeroes.
+        }
+        if (!isZero || !skipZeroes) {
+          //if 1 (if one based)  is the first non-zero entry then print nothing
+          if (hi > onebased || nonzeroes > 0) {
+            if (nonzeroes++ == 0) {
+              dbg.println();//ensure alignment of output, but only if we are definitely outputing something.
+            }
+            dbg.print(hi); dbg.print(":\t"); dbg.println(hist[hi]);
+          }
+          if (andClear) {
+            hist[hi] = 0;
+          }
         }
       }
+      return nonzeroes != 0;
     }
-  }
+
+    bool operator ()(Print &dbg, unsigned modulus, const ShowOptions &forShow) {
+      check();
+      if (!(checks % modulus)) {
+        return show(dbg, forShow);
+      }
+      return false;
+    }
+
 } ;
