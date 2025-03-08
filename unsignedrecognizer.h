f992c1cd36dfe3f8dbac70561bc015d761b39e2a
andyh <andy.980f@gmail.com>
2025-03-07 21:21:52 -0600

    added hex capability,'#' sign makes subsequent digits hex if the number is presently 0 else is a key character

--- unsignedrecognizer.h
+++ unsignedrecognizer.h
@@ -9,27 +9,43 @@
 template<typename Unsigned=unsigned>
 struct UnsignedRecognizer {
   Unsigned accumulator = 0;
+  bool hexly=false; //user can force this, but has to do it before each value entered.
 
-  /** inspect incoming character, @returns whether it is part of the number, If so it has been incorporated into the local number.*/
+  /** inspect incoming character, @returns whether it is part of the number, If so it has been incorporated into the local number.
+    added '#' prefix for hexadecimal strings for entering rgb values for WS281x LED manipulation, 3 8bit numbers packed into 24 out of 32 bits.
+    '~' sets accumulator to all 1's, but you can then add more decimal or hex digits. #~BEEF -> FFFFBEEF
+  */
   bool operator()(char key) {
-    if (Char(key).appliedDigit(accumulator)) {
+    if(key='#' && accumulator==0){//testing accumulator so as to not deal with reinterpreting digits already entered.
+      hexly=true;
+      return true;
+    }
+
+    if (hexly? Char(key).appliedNibble(accumulator) : Char(key).appliedDigit(accumulator)) {
     	return true;
-    } else if(key=='~'){
+    } 
+    
+    if(key=='~'){
     	accumulator = ~0; //to help debug some stuff, delete if it gets bothersome.
     	return true;
-     } else if(key==8 || key ==0x7F){//bs or del
-    	accumulator /= 10;
+    } 
+    
+    if(key==8 || key ==0x7F){//bs or del
+    	if(hexly){
+        accumulator /=16;
+      } else {
+        accumulator /=10;
+      }
     	return true;
-    } else {
-      return false;
-    }
+    } 
+    return false;    
   }
 
 /** @returns whether the incoming number is 0.
    *  not using operator bool() as the compiler preferred to use operator unsigned() instead of oper bool(), so testing the value cleared it.
    * this replaced operator ~ in order to better match other library operator overloading habits, this is obscure enough as it is without being discordant with other uses.
   */
-  bool operator !()const {
+  bool operator !() const {
     return accumulator == 0;
   }
 
@@ -37,6 +53,7 @@ struct UnsignedRecognizer {
   * @returns the present value, setting the local value to zero.
   */
   operator Unsigned() {
+    hexly=false;//reset base with each value extracted
     return take(accumulator);
   }
 };
