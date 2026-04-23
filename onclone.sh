8cf45ac1ea5f3d31d0eb603871f8c9383e0fd817
andyh <andy.980f@gmail.com>
2026-04-23 11:02:13 -0500

    added a link to get Arduino builder to see headers from ezcpp

--- onclone.sh
+++ onclone.sh
@@ -1,3 +1,6 @@
 # when you clone my arduino shared library you also need this other repo. 
 # execute this from the checked out library directory, or do it elsewhere and add a symlink in this library to where the checkout is.
 git clone https://github.com/980f/ezcpp.git
+# added a synonym for ezcpp that lets Arduino include its headers. This will only work for one additional headers library.
+ln -s ezcpp src
+
