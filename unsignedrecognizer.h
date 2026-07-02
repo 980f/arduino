a1f3e8e3ec18b37900cec0145c0dc4b758e2aa9a
andyh <andy.980f@gmail.com>
2026-07-02 17:08:08 -0500

    added distinction between empty and definitely asked for zero

--- unsignedrecognizer.h
+++ unsignedrecognizer.h
@@ -1,14 +1,13 @@
 #pragma once
 
-#include "char.h"  //and to your build add: char.cpp and index.h and cstr.h and textkey.h 
+#include "char.h"  //and to your build also add: char.cpp and index.h and cstr.h and textkey.h 
 /** recognize a sequence of digits. if asked for the value provide it, but clear it. You can peek at the value, but shouldn't except for debugging this module itself.
   leading zeroes are effectively ignored, they do not trigger octal interpretation. */
-
-#include "cheaptricks.h" //take();
 
 template<typename Unsigned = unsigned>
 struct UnsignedRecognizer {
   Unsigned accumulator = 0;
+  bool explicitlyZero = false; //whether a zero char has been entered but no other chars. Added to distinguish 'no entry' from zero 
   bool hexly = false; //user can force this, but has to do it before each value entered.
 
   /** inspect incoming character, @returns whether it is part of the number, If so it has been incorporated into the local number.
@@ -16,17 +15,24 @@ struct UnsignedRecognizer {
     '~' sets accumulator to all 1's, but you can then add more decimal or hex digits. #~BEEF -> FFFFBEEF
   */
   bool operator()(const char key) {
-    if (key == '#' && accumulator == 0) { //testing accumulator so as to not deal with reinterpreting digits already entered.
-      hexly = true;
-      return true;
+    if (key == '#') { 
+      //testing accumulator so as to not deal with reinterpreting digits already entered.
+      if(accumulator == 0 && !explicitlyZero){
+        hexly = true;
+        return true;
+      } else {
+        return false;
+      }      
     }
 
     if (hexly ? Char(key).appliedNibble(accumulator) : Char(key).appliedDigit(accumulator)) {
+      explicitlyZero = accumulator==0;
       return true;
     }
 
-    if (key == '~') {
-      accumulator = ~0; //to help debug some stuff, delete if it gets bothersome.
+    if (key == '~') { //to help debug some stuff, delete this key handler it gets bothersome.
+      accumulator = ~0; 
+      explicitlyZero = false;
       return true;
     }
 
@@ -36,6 +42,7 @@ struct UnsignedRecognizer {
       } else {
         accumulator /= 10;
       }
+      //but we don't update 'explicitlyZero'
       return true;
     }
     return false;
@@ -44,21 +51,21 @@ struct UnsignedRecognizer {
   void clear() {
     hexly = false;
     accumulator = 0;
+    explicitlyZero = false;
   }
 
-  /** @returns whether the incoming number is 0.
-        not using operator bool() as the compiler preferred to use operator unsigned() instead of oper bool(), so testing the value cleared it.
-       this replaced operator ~ in order to better match other library operator overloading habits, this is obscure enough as it is without being discordant with other uses.
-  */
-  bool operator !() const {
-    return accumulator == 0;
+  /** this is a replacement for the operator ! which seemed to occasionally not be used by the compiler over applying ! to operator Unsigned(). 
+    @returns whether some decimal chars were passed to this guy since the last clear() */
+  bool notEmpty() const {
+    return accumulator != 0 || explicitlyZero;
   }
 
   /** look at it and it is gone!
     @returns the present value, setting the local value to zero.
   */
   operator Unsigned() {
-    hexly = false; //reset base with each value extracted
-    return take(accumulator);
+    Unsigned value=accumulator;
+    clear();
+    return value;
   }
 };
